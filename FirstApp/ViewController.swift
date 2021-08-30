9a9bc7ba5a0f7cf5f64a78ec640af2e600e08e5c
weaverth <weaverth@bc.edu>
2021-08-30 18:43:35 -0400

    Week1 You Are Awesome.

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("😎🍆💦 viewDidLoad has executed!")
     }
 
-
+    @IBAction func swallowButtonPressed(_ sender: UIButton) {
+        
+        print("😎🍆💦 swallowButton has been pressed!")
+        messageLabel.text = "Ohhhhhh yeaaaaahhhhh!!!"
+        
+    }
+    
 }
 
