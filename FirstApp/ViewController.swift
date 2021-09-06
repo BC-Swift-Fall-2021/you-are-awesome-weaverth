2fe8f59431757a516b7312ac65e5923a95288271
weaverth <weaverth@bc.edu>
2021-09-06 14:40:52 -0400

    2.8 Done!!!

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -10,23 +10,34 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var awesomeLabel: UILabel!
-    
     @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
         
         
     }
 
     @IBAction func swallowButtonPressed(_ sender: UIButton) {
-        
-        awesomeLabel.text = "Ohhhhhh yeaaaaahhhhh!!"
-        imageView.image = UIImage(named: "image0")
-        
-        
+        let awesomeMessage = "Ohhhhhh yeaaaaahhhhh!!"
+        let greatMessage = "Mmmmmm hmmm!!"
+        let bombMessage = "Bazinga!!!!!"
+    
+        if awesomeLabel.text == awesomeMessage {
+            awesomeLabel.text = greatMessage
+            imageView.image = UIImage(named: "image1")
+        }
+        else if awesomeLabel.text == greatMessage {
+            awesomeLabel.text = bombMessage
+            imageView.image = UIImage(named: "image2")
+        }
+        else {
+            awesomeLabel.text = awesomeMessage
+            imageView.image = UIImage(named: "image0")
+        }
+             
+            
     }
     
 }
